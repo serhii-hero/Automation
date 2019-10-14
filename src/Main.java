459d5c9a93b7fe6fed6629b48f50c6463e6be400
serhii <serhiiherasymovych+1@gmail.com>
2019-10-14 21:38:07 +0300

    first commit from intellij

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        System.out.println("how are you?");
+        System.out.println("Hey");
+        System.out.println("hey");
+    }
+}

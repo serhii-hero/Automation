911738d5e42feea2eb80ddf64109a0e5fbfc7197
serhii <serhiiherasymovych+1@gmail.com>
2019-10-19 09:39:50 +0300

    revert commit from intellij

--- src/CW/uit/CW1.java
+++ src/CW/uit/CW1.java
@@ -4,5 +4,6 @@ public class CW1 {
     public static void main (String [] args) {
         System.out.println("Hi");
         System.out.println("Hi1");
+        System.out.println("Hi2");
     }
 }

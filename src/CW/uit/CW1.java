ccab9144cb6380a0d4c7dccb0ece14579035b4ff
serhii <serhiiherasymovych+1@gmail.com>
2019-10-19 10:44:10 +0300

    2 test commit (master branch)

--- src/CW/uit/CW1.java
+++ src/CW/uit/CW1.java
@@ -4,5 +4,6 @@ public class CW1 {
     public static void main (String [] args) {
         System.out.println("Hi");
         System.out.println("Hi - 1");
+        System.out.println("Hi - 2");
     }
 }

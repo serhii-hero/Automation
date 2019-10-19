310e232a0c18b980ae9ea0839c90a6a695330848
serhii <serhiiherasymovych+1@gmail.com>
2019-10-19 09:46:25 +0300

    fifth commit from intellij

--- src/CW/uit/CW1.java
+++ src/CW/uit/CW1.java
@@ -6,5 +6,11 @@ public class CW1 {
         System.out.println("Hi1");
         System.out.println("Hi2");
         System.out.println("Hi3");
+
+
+        System.out.println("Hi4");
+        System.out.println("Hi5");
+        System.out.println("Hi6");
+        System.out.println("Hi7");
     }
 }
